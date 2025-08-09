2c7b15073442df7bb7135947c1ddcfb0a5706255
seoyoungchae0415 <seoyoungchae0415@gmail.com>
2025-08-09 02:41:56 -0500

    Initial commit

---
+++ Scripts/camera_controller.gd
@@ -0,0 +1,50 @@
+extends Node
+
+signal set_cam_rotation(_cam_rotation : float)
+
+@onready var yaw_node = $CamYaw
+@onready var pitch_node = $CamYaw/CamPitch
+@onready var camera = $CamYaw/CamPitch/Camera3D
+
+var yaw : float = 0
+var pitch : float = 0
+
+var yaw_sensitivity : float = 0.07
+var pitch_sensitivity : float = 0.07
+
+var yaw_acceleration : float = 15
+var pitch_acceleration : float = 15
+
+var pitch_max = 75
+var pitch_min = -75
+
+var tween : Tween
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		yaw += -event.relative.x * yaw_sensitivity
+		pitch += event.relative.y * pitch_sensitivity
+	
+
+func _physics_process(delta):
+	pitch = clamp(pitch, pitch_min, pitch_max)
+	
+	# Lerp function for smooth camera
+	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, yaw_acceleration * delta)
+	pitch_node.rotation_degrees.x = lerp(pitch_node.rotation_degrees.x, pitch, pitch_acceleration * delta)
+	
+	# Snappy Camera
+	# yaw_node.rotation_degrees.y = yaw
+	# pitch_node.rotation_degrees.x = pitch
+	
+	set_cam_rotation.emit(yaw_node.rotation.y)
+
+func _on_set_movement_state(_movement_state : MovementState):
+	if tween:
+		tween.kill()
+	
+	tween = create_tween()
+	tween.tween_property(camera, "fov", _movement_state.camera_fov, 0.5).set_trans(Tween. TRANS_SINE).set_ease(Tween.EASE_OUT)
---
+++ player.gd
@@ -0,0 +1,58 @@
+extends CharacterBody3D
+
+signal pressed_jump(jump_state : JumpState)
+signal set_movement_state(_movement_state: MovementState)
+signal set_movement_direction(_movement_direction: Vector3)
+
+@export var movement_states : Dictionary
+@export var max_air_jump : int = 5
+@export var jump_states : Dictionary
+
+var air_jump_counter : int = 0
+var movement_direction : Vector3
+
+func _ready():
+	set_movement_state.emit(movement_states["stand"])
+
+func _input(event):
+	if event.is_action("movement"):
+		movement_direction.x = Input.get_action_strength("left") - Input.get_action_strength("right")
+		movement_direction.z = Input.get_action_strength("forward") - Input.get_action_strength("back")
+		
+		if is_movement_ongoing():
+			if Input.is_action_pressed("sprint"):
+				set_movement_state.emit(movement_states["sprint"])
+			else:
+				if Input.is_action_pressed("walk"):
+					set_movement_state.emit(movement_states["walk"])
+				else:
+					set_movement_state.emit(movement_states["run"])
+		else:
+			set_movement_state.emit(movement_states["stand"])
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
+func _physics_process(delta):
+	if is_movement_ongoing():
+		set_movement_direction.emit(movement_direction)
+	
+	if is_on_floor():
+		air_jump_counter = 0
+	elif air_jump_counter == 0:
+		air_jump_counter = 1
+	
+	if air_jump_counter <= max_air_jump:
+		if Input.is_action_just_pressed("jump"):
+			var jump_name = "Jump_Start"
+			
+			if air_jump_counter > 0:
+				jump_name = "air_jump"
+			
+			pressed_jump.emit(jump_states[jump_name])
+			air_jump_counter += 1
+
+func is_movement_ongoing() -> bool:
+	return abs(movement_direction.x) > 0 or abs(movement_direction.z) > 0
+
+func _set_movement_state(state : String):
+	set_movement_state.emit(movement_states[state])
